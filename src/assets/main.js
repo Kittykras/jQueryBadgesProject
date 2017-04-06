e1e2377b500450ad29f16aedb45349ea1da79d56
unknown <asu@ALPHA-ASU-T560.asol.local>
2017-04-06 10:41:35 +0200

    Add Ajax request to insert codeschool completed courses

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
-
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/AnnetteSund.json',
+        dataType: 'jsonp',
+        success: function(response) {
+            var completed = response.courses.completed;
+            $.each(completed, function(index, item) {
+                $('#badges').append('<div class="course">' +
+                    '<h3>' +
+                    item.title +
+                    '</h3>' +
+                    '<img src="' +
+                    item.badge +
+                    '"/>' +
+                    '<a class="btn btn-primary" href="' +
+                    item.url +
+                    '" target="_blank">See Course</a>' +
+                    '</div>');
+            });
+        }
+    });
+    
 
 });
